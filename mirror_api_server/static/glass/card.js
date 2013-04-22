a7d40253812f11a93fc9ff2d7d218171c7a2aee3
Scarygami <scarygami@gmail.com>
2013-04-23 00:46:44 +0200

    Interpret glass map image urls

--- mirror_api_server/static/glass/card.js
+++ mirror_api_server/static/glass/card.js
@@ -35,8 +35,93 @@
     }
   }
 
+  function renderMap(image) {
+    var src, parts, part, i, l, width = 640, height = 360, elements = [], marker, coords, polyline, polystyle, tmp;
+
+    src = image.src;
+
+    if (src.indexOf("glass://map?") !== 0) { return; }
+
+    src = src.substring(12);
+
+    parts = src.split("&");
+
+    l = parts.length;
+    for (i = 0; i < l; i++) {
+      part = parts[i].split("=");
+      if (part.length !== 2) { continue; }
+      switch (part[0]) {
+      case "w":
+        width = part[1];
+        break;
+      case "h":
+        height = part[1];
+        break;
+      case "marker":
+        marker = part[1].split(";");
+        if (marker.length === 2) {
+          coords = marker[1].split(",");
+          if (coords.length === 2) {
+            elements.push("markers=color:0xF7594A|" + coords[0] + "," + coords[1]);
+          }
+        }
+        break;
+      case "polyline":
+        polyline = part[1].split(";");
+        if (polyline.length === 2) {
+          polystyle = polyline[0].split(",");
+          if (polystyle.length === 2) {
+            /**
+             * According to documentation ffff0000 results in a red line,
+             * meaning that FF is the opacity followed by the RGB Values FF0000
+             * representation in Google Static maps is the other way round (RGBA)
+             * Unless of course the documentation is wrong, but I'll go by that for now
+             */
+            if (polystyle[1].length !== 8) {
+              polystyle[1] = "0x1871ADFF";
+            } else {
+              polystyle[1] = "0x" + polystyle[1].substring(2) + polystyle[1].substring(0, 2);
+            }
+          } else {
+            polystyle = ["8", "0x1871ADFF"];
+          }
+          coords = polyline[1].split(",");
+          if (coords.length > 0) {
+            tmp = "path=color:" + polystyle[1] + "|weight:" + polystyle[0];
+            l = coords.length;
+            for (i = 0; i < l; i += 2) {
+              // skipping last coord in case odd number given
+              if (i !== l - 1) {
+                tmp += "|" + coords[i] + "," + coords[i + 1];
+              }
+            }
+            elements.push(tmp);
+          }
+        }
+        break;
+      case "center":
+        elements.push("center=" + part[1]);
+        break;
+      case "zoom":
+        elements.push("zoom=" + part[1]);
+        break;
+      }
+    }
+
+    src =
+      "https://maps.googleapis.com/maps/api/staticmap?sensor=false&size=" + width + "x" + height +
+      "&style=feature:all|element:all|saturation:-100|lightness:-25|gamma:0.5|visibility:simplified" +
+      "&style=feature:roads|element:geometry&style=feature:landscape|element:geometry|lightness:-25";
+
+    if (elements.length > 0) {
+      src += "&" + elements.join("&");
+    }
+
+    image.src = src;
+  }
+
   global.setData = function (text, image, html, date) {
-    var tmpDiv, timeDiv, resize, i;
+    var tmpDiv, timeDiv, resize, i, images;
     if (!html) {
       // HTML Contents overwrite all other contents, otherwise create html from text and image
       if (!!image) {
@@ -64,6 +149,13 @@
     resize = cardDiv.querySelectorAll(".text-auto-size");
     for (i = 0; i < resize.length; i++) {
       autoResizeText(resize[i]);
+    }
+
+    images = cardDiv.querySelectorAll("img");
+    for (i = 0; i < images.length; i++) {
+      if (images[i].src.indexOf("glass://map?") === 0) {
+        renderMap(images[i]);
+      }
     }
   };
 
--- mirror_api_server/static/glass/glass.js
+++ mirror_api_server/static/glass/glass.js
@@ -186,6 +186,12 @@
           "created": "2013-04-12T15:31:41.000000",
           "updated": "2013-04-12T15:31:41.000000",
           "id": 1
+        },
+        {
+          "id": 8,
+          "created": "2013-04-22T16:00:00.000000",
+          "updated": "2013-04-22T16:00:00.000000",
+          "html": "<article><figure><img src=\"glass://map?w=240&h=360&marker=0;42.369590,-71.107132&marker=1;42.36254,-71.08726&polyline=;42.36254,-71.08726,42.36297,-71.09364,42.36579,-71.09208,42.3697,-71.102,42.37105,-71.10104,42.37067,-71.1001,42.36561,-71.10406,42.36838,-71.10878,42.36968,-71.10703\" height=\"360\" width=\"240\"></figure><section><div class=\"text-auto-size\"><p class=\"yellow\">12 minutes to home</p><p>Medium traffic on Broadway</p></div></section></article>"
         }
       ]
     };
