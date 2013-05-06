07521dd1259fb34032e3306cbdb5ff104a5f7150
Scarygami <scarygami@gmail.com>
2013-05-06 13:13:50 +0200

    Add Hangout authentication

--- examples/hangout_companion/web/hangout_companion.dart
+++ examples/hangout_companion/web/hangout_companion.dart
@@ -2,16 +2,22 @@ import "dart:html";
 import "dart:async";
 import "dart:json" as JSON;
 import "package:hangouts_api/hangouts_api.dart";
+import "src/HangoutOAuth2.dart";
 
-var hapi;
+Hangout hapi;
+HangoutOAuth2 auth;
+
+void initialize() {
+  auth = new HangoutOAuth2([]);
+  auth.login().then((t) {
+    print(t);
+  });
+}
 
 void main() {
-  hapi = new Hangout();
-  
-  hapi.onApiReady.add((ApiReadyEvent event) {
-    if (event.isApiReady) {
-      // DO someting
-    }
+  var hapi = new Hangout();
+  hapi.onApiReady.add((e) {
+    if (e.isApiReady) initialize();
   });
 }
 
