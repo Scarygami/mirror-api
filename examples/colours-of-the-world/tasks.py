6c4a8a2333f4babbfd75264cc0f04649e9c7d197
Scarygami <scarygami@gmail.com>
2013-05-22 19:18:45 +0200

    Colour detection

--- examples/colours-of-the-world/tasks.py
+++ examples/colours-of-the-world/tasks.py
@@ -15,7 +15,6 @@
 
 """Create tasks and evaluate submissions, implemented as taskqueues to work in the background"""
 
-import upload
 from auth import get_auth_service
 from models import Submission
 from utils import base_url
@@ -31,160 +30,171 @@ from google.appengine.ext import ndb
 from oauth2client.client import AccessTokenRefreshError
 
 COLORS = {
-    "red": {"name": "Red", "hue": 0, "range": 15},
-    "orange": {"name": "Orange", "hue": 30, "range": 15},
-    "yellow": {"name": "Yellow", "hue": 60, "range": 15},
-    "green": {"name": "Green", "hue": 120, "range": 15},
-    "blue": {"name": "Blue", "hue": 240, "range": 15},
-    "indigo": {"name": "Indigo", "hue": 280, "range": 15},
-    "violet": {"name": "Violet", "hue": 320, "range": 15}
+    "red": {"name": "Red", "hue": 0, "min": -15, "max": 15},
+    "orange": {"name": "Orange", "hue": 30, "min": 15, "max": 45},
+    "yellow": {"name": "Yellow", "hue": 60, "min": 45, "max": 90},
+    "green": {"name": "Green", "hue": 120, "min": 90, "max": 180},
+    "blue": {"name": "Blue", "hue": 240, "min": 180, "max": 260},
+    "indigo": {"name": "Indigo", "hue": 280, "min": 250, "max": 310},
+    "violet": {"name": "Violet", "hue": 320, "min": 300, "max": 345}
 }
 
 
 class CreateTaskWorker(webapp2.RequestHandler):
-  """
-  Creates a new task for a user
-  """
- 
-  def post(self):
-      
-    gplus_id = self.request.get("user")
-    test = self.request.get("test")
-    if test == "":
-        test = None
+    """
+    Creates a new task for a user
+    """
 
-    service = get_auth_service(gplus_id, test)
-    if service is None:
-        logging.error("User not authenticated")
-        return
-    
-    if test is not None:
-        user = ndb.Key("TestUser", gplus_id).get()
-    else:
-        user = ndb.Key("User", gplus_id).get()
-    
-    if user is None:
-        logging.error("User not found")
-        return
-        
-    col = random.choice(COLORS.keys())
-    
-    user.currentTask = col
-    user.put()
-    
-    card = {
-        "html": ("<article class=\"photo\">"
-                 "  <img src=\"" + base_url + "/images/" + col + ".png\" width=\"100%\" height=\"100%\">"
-                 "  <div class=\"photo-overlay\"></div>"
-                 "  <section>"
-                 "    <p class=\"text-auto-size\">Current Task: " + COLORS[col]["name"] + "</p>"
-                 "  </section>"
-                 "</article>"),
-        "menuItems": [
-            {
-                "action": "CUSTOM",
-                "id": "giveup",
-                "values": [{
-                  "displayName": "Give Up",
-                  "iconUrl": "https://mirror-api.appspot.com/glass/images/error.png"
-                }]
-            },
-            {
-                "action": "TOGGLE_PINNED"
-            }
-        ]
-    }
+    def post(self):
 
-    try:
-        service.timeline().insert(body=card).execute()
-    except AccessTokenRefreshError:
-        logging.error("Failed to refresh access token.")
-        return
-    except HttpError as e:
-        logging.error("Failed to execute request. %s" % e)
-        return    
+        gplus_id = self.request.get("user")
+        test = self.request.get("test")
+        if test == "":
+            test = None
+
+        service = get_auth_service(gplus_id, test)
+        if service is None:
+            logging.error("User not authenticated")
+            return
+
+        if test is not None:
+            user = ndb.Key("TestUser", gplus_id).get()
+        else:
+            user = ndb.Key("User", gplus_id).get()
+
+        if user is None:
+            logging.error("User not found")
+            return
+
+        col = random.choice(COLORS.keys())
+
+        user.currentTask = col
+        user.put()
+
+        card = {
+            "html": ("<article class=\"photo\">"
+                     "  <img src=\"" + base_url + "/images/" + col + ".png\" width=\"100%\" height=\"100%\">"
+                     "  <div class=\"photo-overlay\"></div>"
+                     "  <section>"
+                     "    <p class=\"text-auto-size\">Current Task: " + COLORS[col]["name"] + "</p>"
+                     "  </section>"
+                     "</article>"),
+            "menuItems": [
+                {
+                    "action": "CUSTOM",
+                    "id": "giveup",
+                    "values": [{
+                        "displayName": "Give Up",
+                        "iconUrl": "https://mirror-api.appspot.com/glass/images/error.png"
+                    }]
+                },
+                {
+                    "action": "TOGGLE_PINNED"
+                }
+            ]
+        }
+
+        try:
+            service.timeline().insert(body=card).execute()
+        except AccessTokenRefreshError:
+            logging.error("Failed to refresh access token.")
+            return
+        except HttpError as e:
+            logging.error("Failed to execute request. %s" % e)
+            return
 
 
 class EvaluateWorker(webapp2.RequestHandler):
-  """
-  Creates a new task for a user
-  """
- 
-  def post(self):
-      
-    gplus_id = self.request.get("user")
-    test = self.request.get("test")
-    if test == "":
-        test = None
+    """
+    Creates a new task for a user
+    """
 
-    item_id = self.request.get("item")
-    
-    service = get_auth_service(gplus_id, test)
+    def post(self):
 
-    if service is None:
-        logging.error("No valid credentials")
-        return
+        gplus_id = self.request.get("user")
+        test = self.request.get("test")
+        if test == "":
+            test = None
 
-    item = service.timeline().get(id=item_id).execute()
-    
-    imageId = None
-    if "attachments" in item:
-        for att in item["attachments"]:
-            if att["contentType"].startswith("image/"):
-                imageId = att["id"]
-                break
+        item_id = self.request.get("item")
 
-    if imageId is None:
-        logging.info("No suitable attachment")
-        return
+        service = get_auth_service(gplus_id, test)
 
-    attachment_metadata = service.timeline().attachments().get(
-        itemId=item["id"], attachmentId=imageId).execute()
-    content_url = attachment_metadata.get("contentUrl")
-    resp, content = service._http.request(content_url)
+        if service is None:
+            logging.error("No valid credentials")
+            return
 
-    if resp.status != 200:
-        logging.info("Couldn't fetch attachment")
+        item = service.timeline().get(id=item_id).execute()
 
-    tempimg = cStringIO.StringIO(content)
-    i = Image.open(tempimg).convert("RGB")
-    a = numpy.asarray(i, int)
+        imageId = None
+        if "attachments" in item:
+            for att in item["attachments"]:
+                if att["contentType"].startswith("image/"):
+                    imageId = att["id"]
+                    break
 
-    R, G, B = a.T
+        if imageId is None:
+            logging.info("No suitable attachment")
+            return
 
-    m = numpy.min(a,2).T
-    M = numpy.max(a,2).T
+        attachment_metadata = service.timeline().attachments().get(
+            itemId=item["id"], attachmentId=imageId).execute()
+        content_url = attachment_metadata.get("contentUrl")
+        resp, content = service._http.request(content_url)
 
-    C = M-m #chroma
-    Cmsk = C!=0
+        if resp.status != 200:
+            logging.info("Couldn't fetch attachment")
 
-    # Hue
-    H = numpy.zeros(R.shape, int)
-    mask = (M==R)&Cmsk
-    H[mask] = numpy.mod(60*(G-B)/C, 360)[mask]
-    mask = (M==G)&Cmsk
-    H[mask] = (60*(B-R)/C + 120)[mask]
-    mask = (M==B)&Cmsk
-    H[mask] = (60*(R-G)/C + 240)[mask]
+        tempimg = cStringIO.StringIO(content)
+        i = Image.open(tempimg).convert("RGB").resize((200, 200), Image.ANTIALIAS)
+        a = numpy.asarray(i, int)
 
-    # Value
-    V = M
+        R, G, B = a.T
 
-    # Saturation
-    S = numpy.zeros(R.shape, int)
-    S[Cmsk] = ((255*C)/V)[Cmsk]
-    
-    j = Image.fromarray(H)
-    f = cStringIO.StringIO()
-    j.save(f, "PNG")
-    content = f.getvalue()
-    f.close()
+        m = numpy.min(a, 2).T
+        M = numpy.max(a, 2).T
 
-    new_item = {}
-    new_item["menuItems"] = [{"action": "SHARE"}]
+        #Chroma
+        C = M - m
+        Cmsk = C != 0
 
-    result = upload.multipart_insert(new_item, content, "image/png", service, test)
-    logging.info(result)
+        # Hue
+        H = numpy.zeros(R.shape, int)
+        mask = (M == R) & Cmsk
+        H[mask] = numpy.mod(60*(G-B)/C, 360)[mask]
+        mask = (M == G) & Cmsk
+        H[mask] = (60*(B-R)/C + 120)[mask]
+        mask = (M == B) & Cmsk
+        H[mask] = (60*(R-G)/C + 240)[mask]
+
+        # Value
+        V = M
+
+        # Saturation
+        S = numpy.zeros(R.shape, int)
+        S[Cmsk] = ((255*C)/V)[Cmsk]
+
+        mask = (V > 100) & (S > 100)
+
+        count = {}
+        for col in COLORS:
+            count[col] = 0
+            v1 = COLORS[col]["min"]
+            v2 = COLORS[col]["max"]
+            if (v1 < 0):
+                col_mask = ((H < v2) | (H > 360 + v1)) & mask
+            else:
+                col_mask = ((H > v1) & (H < v2)) & mask
+            Col = numpy.zeros(R.shape, int)
+            Col[col_mask] = numpy.ones(R.shape, int)[col_mask]
+            count[col] = numpy.count_nonzero(Col)
+
+        logging.info(count)
+        #new_item = {}
+        #new_item["menuItems"] = [{"action": "SHARE"}]
+
+        #result = upload.multipart_insert(new_item, content, "image/png", service, test)
+        #logging.info(result)
 
 
 TASK_ROUTES = [
